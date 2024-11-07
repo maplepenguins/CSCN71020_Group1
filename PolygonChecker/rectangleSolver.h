67b6a0fb362c9e1e750a910b04969ddb15f313f9
Ahmed Somaan <ahmedsomaan7@gmail.com>
2024-11-07 10:24:52 -0500

    created struct for points for rectangle and calculated rectangle side lengths based on array of 4 points

--- PolygonChecker/rectangleSolver.h
+++ PolygonChecker/rectangleSolver.h
@@ -1,2 +1,9 @@
 #pragma once
+typedef struct Point {
+	int x;
+	int y;
+} Point;
+
+double* calculateRectangleSides(Point* rectanglePoints[]);
 char* analyzeRectangle(int side1, int side2, int side3, int side4);
+
