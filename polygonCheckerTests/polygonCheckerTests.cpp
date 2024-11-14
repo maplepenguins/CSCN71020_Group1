99cf257e0424f2cb8567af5a330bbbc9718f4c68
Michael <mthomson9322@conestogac.on.ca>
2024-11-14 11:13:26 -0500

    Added test project

---
+++ polygonCheckerTests/polygonCheckerTests.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace polygonCheckerTests
+{
+	TEST_CLASS(typeOfTriangleTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+
+	TEST_CLASS(triangleInsideAngleTests)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+
+	};
+
+	TEST_CLASS(fourpoints)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
