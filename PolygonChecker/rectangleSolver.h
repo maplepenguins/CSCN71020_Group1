25766175cec6f324a5f370bde204f3e7dd94f652
Ahmed Somaan <ahmedsomaan7@gmail.com>
2024-11-21 00:05:48 -0500

    updated headers

--- PolygonChecker/rectangleSolver.h
+++ PolygonChecker/rectangleSolver.h
@@ -7,8 +7,6 @@ typedef struct Point {
 float calculateDistance(Point p1, Point p2);
 bool isRectangle(Point rectanglePoints[4]);
 char* analyzeRectangle(Point rectanglePoints[4]);
-double* calculateRectangleSides(Point* rectanglePoints[]);
-char* analyzeRectangle(double* sideLengths[]);
 double calculateRectangleArea(double* sideLengths);
 double calculateRectanglePerimeter(Point* rectanglePoints[]);
 
